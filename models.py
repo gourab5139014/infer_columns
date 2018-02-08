6422799cbb2af9e2fa322f9a3b18be5ae24db7c0
gourabmi <gourabmi@buffalo.edu>
2018-02-08 13:43:04 -0500

    skeleton files2

---
+++ models.py
@@ -0,0 +1,9 @@
+# Maintain the metadata of the column
+class Column:
+    def __init__(self):
+        self.idx = 0 #TODO Change this later
+        
+# Row of a dataset
+class Row:
+    def __init__(self, d):
+        self.data = d
