f0a73c7646bff2e46c6d4aa24097b65daeb90a38
gourabmi <gourabmi@buffalo.edu>
2018-02-08 16:44:31 -0500

    saving progress

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
+class Dataset:
+    def __init__(self, size):
+        self.columns = [Column() for i in range(0, size) ]
+
+    def __    
 # Maintain the metadata of the column
 class Column:
     def __init__(self):
         self.idx = 0 #TODO Change this later
-        
+
 # Row of a dataset
-class Row:
+class Row: #TODO Not sure if I want to use this anymore
     def __init__(self, d):
         self.data = d
