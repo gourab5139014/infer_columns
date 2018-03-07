c56254de3c128124ba9ab2004a74136818d93e41
gourabmi <gourabmi@buffalo.edu>
2018-03-06 20:40:06 -0500

    Starting to build the compare tool

--- script.py
+++ script.py
@@ -1,9 +1,30 @@
-import csv
+# import warnings
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+import statsmodels as sm
+# import matplotlib
+# import matplotlib.pyplot as plt
 
-from models import Row
+# START CONFIGURATION SECTION
 
-with open('total_waterborne_commerce.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for r in readCSV:
-        print r
-    d = Row(1)
+DISTRIBUTIONS = [st.uniform, st.norm, st.zipf]
+
+# END CONFIGURATION SECTION
+def get_uniform_distributed_integers(min_value, max_value, total, fliped):
+    fake1 = np.linspace(min_value, max_value, total, dtype=int)
+    replace_at = np.random.randint(min_value, max_value, size=fliped)
+    for i in range(0, len(fake1)):
+        if(i in replace_at):
+            fake1[i] = np.random.randint(min_value, max_value)
+    return fake1
+
+def study_dataset_from_file(d):
+    dt = pd.read_csv(d)
+    print(dt.head())
+    # TODO Resume by studying MLE based estimation of stat params and validate if our KL div approach is valid
+
+if __name__ == "__main__":
+    datasets = ['total_waterborne_commerce.csv']
+    for d in datasets:
+        study_dataset_from_file(d)
